ee74d76c610d182dab0696f9da598fe8b849bc17
Daniele <daniele.benedetto@outlook.it>
2026-03-13 17:24:02 +0100

    Update category styles to adjust description width for improved responsiveness

--- components/organism/category/styles.ts
+++ components/organism/category/styles.ts
@@ -11,7 +11,7 @@ const styles = {
   header: cn("flex items-center gap-6 mb-4"),
   title: cn("min-w-0 lg:text-[24px] md:text-[20px] text-[20px]"),
   separator: cn("flex-1"),
-  description: cn("flex text-sm lg:w-1/3 md:w-full w-full mb-8"),
+  description: cn("flex text-sm lg:w-2/3 md:w-full w-full mb-8"),
   separatorBottom: cn(""),
 };
 
