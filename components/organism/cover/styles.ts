4a79d209464a7c5c1652301eb424e00bd7df73a6
Daniele <daniele.benedetto@outlook.it>
2026-03-28 13:11:11 +0100

    Update styles in Cover component for improved layout and order
    - Adjusted the order of imageWrapper and footer styles to enhance layout consistency and responsiveness in the Cover component.

--- components/organism/cover/styles.ts
+++ components/organism/cover/styles.ts
@@ -9,14 +9,14 @@ import { cn } from "@/lib/utils/classes";
 const styles = {
   link: cn("h-full"),
   container: cn("h-full flex flex-col"),
-  imageWrapper: cn("flex flex-1 min-h-0 lg:p-0 md:p-0 px-4 pt-0 pb-4 relative order-1"),
+  imageWrapper: cn("flex flex-1 min-h-0 lg:p-0 md:p-0 px-4 pt-0 pb-4 relative order-2 md:order-1"),
   image: cn("w-full h-full object-cover max-h-[500px]"),
   badgesWrapper: cn("absolute top-8 left-8 lg:flex md:flex hidden"),
   badgeDate: cn("bg-secondary py-1 px-2 w-fit"),
   badgeTitle: cn("bg-primary py-1 px-2 w-fit"),
   badgeTextDate: cn("text-xs! md:text-xs! lg:text-base! text-primary"),
   badgeTextTitle: cn("text-xs! md:text-xs! lg:text-base!"),
-  footer: cn("flex gap-2 shrink-0 order-2"),
+  footer: cn("flex gap-2 shrink-0 order-1 md:order-2"),
 };
 
 /* **************************************************
