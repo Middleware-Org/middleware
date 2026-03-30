8ee6213bc5387b1c7506c25749d00639af234734
Daniele <daniele.benedetto@outlook.it>
2026-03-30 09:12:52 +0200

    fIx

--- app/[locale]/podcast/[slug]/page.tsx
+++ app/[locale]/podcast/[slug]/page.tsx
@@ -282,7 +282,7 @@ export default async function PodcastPage({ params }: PodcastPageProps) {
           <div className="max-w-[1472px] mx-auto lg:px-10 md:px-4 px-4">
             <Separator />
           </div>
-          <section className="max-w-[1472px] mx-auto lg:px-10 md:px-4 px-4 pb-0 pt-5">
+          <section className="max-w-[1472px] mx-auto lg:px-10 md:px-4 px-4 pb-0 pt-[25px]">
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
               {relatedPodcasts.map((relatedPodcast) => (
                 <PodcastCard
